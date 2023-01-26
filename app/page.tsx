5a9292351654fc134460eb518b2fff643ee2072b
Gianik <paoloduguran@gmail.com>
2023-01-26 20:33:43 +0800

    init sidebar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-
+import Sidebar from "./Sidebar"
 
 function HomePage() {
     return (
       
       <div className="flex" >
             <h1 className="text-green-500 text font-bold">This is the HomePage</h1>
-
+            <Sidebar />
       </div>
   )
 }
