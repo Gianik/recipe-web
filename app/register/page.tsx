69fece099505bc86470b23da5c306b488bc1da99
Gianik <paoloduguran@gmail.com>
2023-01-27 22:52:23 +0800

    added create api call

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -3,26 +3,50 @@
 import React, { FormEvent, useState } from "react";
 import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/solid";
 import { useRouter } from "next/navigation";
-
+import PocketBase from 'pocketbase';
 
 
 export default function LoginPage() {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [confirmPassword,setConfirmPassword] = useState("")
     const [fullName, setFullName] = useState("");
     const [showPassword, setShowPassword] = useState(false);
-    
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+    const [errorMessage,setErrorMessage] = useState("Test Error")
     //handle the form submit to login user
     const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-        console.log("Email:" + email)
-        console.log("Password:" + password)
-        console.log("Full Name:" + fullName)
+        const pb = new PocketBase('http://127.0.0.1:8090');
+
+        let valid = validateInput()
+
+        if (valid == true) {
+            const record = await pb.collection('users').create({
+                name: fullName,
+                email: email,
+                password: password,
+                passwordConfirm: confirmPassword,
+                role: 'user'
+            }).then(data => {
+                console.log(data)
+            })
+                .catch(error => {
+                    console.log(error)
+                })
+                setEmail("")
+                setPassword("")
+                setConfirmPassword("")
+                setFullName("")
+        }
+
         setEmail("")
         setPassword("")
+        setConfirmPassword("")
         setFullName("")
     };
+    //handle showPassword State
     const handleShowPassword = (e:any) => {
         e.preventDefault()
         if (showPassword === true) {
@@ -35,6 +59,23 @@ export default function LoginPage() {
         }
 
     };
+    //change showConfirmPassword state
+    const handleShowConfirmPassword = (e:any) => {
+        e.preventDefault()
+        if (showPassword === true) {
+            setShowConfirmPassword(false)
+            return
+        }
+        else {
+            setShowConfirmPassword(true)
+            return
+        }
+
+    };
+    //validate input of user
+    const validateInput = () => {
+        return false
+    }
 
 
     return (
@@ -49,8 +90,11 @@ export default function LoginPage() {
                         </div>
                         <div>
                             <p className="text-xs mt-4">Register to Recipe Blog</p>
+                            {/* <div>
+                                <h3>{ errorMessage }</h3>
+                            </div> */}
 
-                            <form onSubmit={handleSubmit} className="flex flex-col gap-4 mt-2">
+                            <form onSubmit={handleSubmit} className="flex flex-col gap-4 mt-2" noValidate>
                                 <input
                                     type="email"
                                     className="p-1 mt-4 rounded-lg text-black"
@@ -67,6 +111,16 @@ export default function LoginPage() {
                                         onChange={(e) => setPassword(e.target.value)}
                                     />
                                     <button onClick={handleShowPassword}>{showPassword ? (<EyeIcon className="w-4 h-4 absolute top-1/2 right-3 -translate-y-1/2" />) : (<EyeSlashIcon className="w-4 h-4 absolute top-1/2 right-3 -translate-y-1/2" />)}</button>
+                                </div>
+                                <div className="relative">
+                                    <input
+                                        type={showConfirmPassword ? "text": "password"}
+                                        className="p-1 rounded-lg text-black"
+                                        value={confirmPassword}
+                                        placeholder="Confirm Password"
+                                        onChange={(e) => setConfirmPassword(e.target.value)}
+                                    />
+                                    <button onClick={handleShowConfirmPassword}>{showConfirmPassword ? (<EyeIcon className="w-4 h-4 absolute top-1/2 right-3 -translate-y-1/2" />) : (<EyeSlashIcon className="w-4 h-4 absolute top-1/2 right-3 -translate-y-1/2" />)}</button>
                                 </div>
                                 <input
                                     type="text"
