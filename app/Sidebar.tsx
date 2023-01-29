27e50efdccfe41869f016d90c395cd34c6b4c9c6
Gianik <paoloduguran@gmail.com>
2023-01-29 23:14:07 +0800

    added link to profile

--- app/Sidebar.tsx
+++ app/Sidebar.tsx
@@ -40,7 +40,7 @@ const SideBar = forwardRef(({ showNav }:any, ref:any) => {
             </div>
           </div>
         </Link>
-        <Link href="#">
+        <Link href="/profile">
           <div
             className={`pl-6 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors`}
           >
