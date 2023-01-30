cd2c107f41e0ff95480d44d6e9bd34859a9701a0
Gianik <paoloduguran@gmail.com>
2023-01-30 14:04:32 +0800

    added user view

---
+++ app/user/[userId]/page.tsx
@@ -0,0 +1,42 @@
+
+
+import {  PlusCircleIcon,PencilSquareIcon,TrashIcon} from "@heroicons/react/24/solid";
+import PocketBase from 'pocketbase';
+import Link from "next/link";
+import Dashboard from "../../dashboard";
+
+async function getUser(userId: string) {
+        const pb = new PocketBase(process.env.PB_LINK);
+        const data = await pb.collection('users').getOne(`${userId}`)
+
+        return data
+}
+
+export default async  function  HomePage({ params }: any){
+    const user = await getUser(params.userId)
+
+    
+    return (
+            <>
+                <Dashboard children={undefined} />
+                <div className="flex mt-2 text-center justify-center">
+                    <h2 className="font-bold pl-1 text-3xl mt-2">My Recipes</h2>
+                    <Link href={`/editusers/${user.id}`}><button className="bg-yellow-500 text-white ml-3 mt-4  px-2 rounded-lg flex"> Edit User   <PencilSquareIcon className="w-6 h-6 pl-1 top-1/2 right-0 " /> </button></Link>
+                    <Link href={`/deleteuser/${user.id}`}><button className="bg-red-500 text-white ml-3 mt-4  px-2 rounded-lg flex"> Delete User   <TrashIcon className="w-6 h-6 pl-1 top-1/2 right-0 " /> </button></Link>
+                </div>
+                <div className="flex  flex-grow items-center text-center justify-center gap-5 ml-[100px] m-[100px] mt-16 text-white">
+                    <div  className="rounded-xl flex  flex-col grid-cols-1   bg-[#363740]  w-[500px] text-white shadow-sm  " >
+                        <h2 className=" mt-[0px] capitalize text-center">Name: {user.name}</h2>
+                        
+                        <hr className="w-full mt-1" />
+                        <h2 className=" mt-[0px] text-center">Email: {user.email}</h2>
+                        <hr className="w-full mt-1" />
+                        <h2 className=" mt-[0px] capitalize text-center">Role: {user.role}</h2>
+
+                    </div>
+                </div>
+            </>
+
+        
+      );
+    }
